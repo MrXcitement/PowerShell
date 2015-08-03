0a89d53f50b5be20bb778da25ad97221230aab92
Mike Barker <mike@thebarkers.com>
2015-08-03 15:10:47 -0600

    Personal power shell profile.

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,8 @@
+# Microsoft.PowershellISE_profile.ps1 --- Default powershell ISE config
+#
+# Mike Barker <mike@thebarkers.com>
+# June 26th, 2015
+#
+
+# Source the default profile
+. $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
