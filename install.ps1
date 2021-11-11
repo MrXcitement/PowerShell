4bddd8af22935e4682a1d6017f3714d0254c0671
Mike Barker <mike@thebarkers.com>
2021-11-11 10:58:53 -0700

    Fix install issue
    Changes made to handle Windows and Core versions of Powershell

--- install.ps1
+++ install.ps1
@@ -26,8 +26,16 @@ Function New-SymbolicLink([string]$link, [string]$target) {
 }
 
 # Test-Elevated - Test if the current powershell session is being run with elevated permisions
+# Return False on systems that do not have the method defined ie. non windows hosts
 Function Test-Elevated() {
-    return [Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544'
+    try {
+        $is_elevated = [Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544'
+    }
+    catch [MethodInvocationException] {
+        # This method does not exist on non windows hosts, assume not elevated
+        $is_elevated = False
+    }
+    return $is_elevated
 }
 
 # Test-SymbolicLink - Test if the path is a symbolic link file
@@ -36,8 +44,14 @@ Function Test-SymbolicLink([string]$path) {
     Return [bool]($file.LinkType -eq "SymbolicLink")
 }
 
+# IsWindows variable is not defined in Windows Powershell 5.x, 
+# so define a local variable and set it to True
+if (-Not (Get-Variable IsWindows -Scope Global -ErrorAction SilentlyContinue )) {
+    $IsWindows = True
+}
+
 # Verify the script is being run with elevated permisions on Windows
-if (($PSVersionTable.Platform -EQ "Windows" ) -And (-Not (Test-Elevated))) {
+if (($IsWindows) -And (-Not (Test-Elevated))) {
     throw "This script must be run 'Elevated' as an Administrator"
 }
 
